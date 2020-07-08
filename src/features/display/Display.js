4b9e9f5c6ea4848dedec41fbdc5292466fab773b
barteklecki <leckibartek@gmail.com>
2020-07-08 10:39:48 +0200

    refactor(global): cleanup console.log's

--- src/features/display/Display.js
+++ src/features/display/Display.js
@@ -6,9 +6,6 @@ import { selectOperations, selectResult } from './displaySlice';
 const Display = () => {
     const operations = useSelector(selectOperations);
     const result = useSelector(selectResult);
-    console.log(selectResult);
-    console.log(result);
-    console.log(store => store);
 
     return (
         <div>
