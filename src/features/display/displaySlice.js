dad4bb5f60f0f671659816fcc61295bde9ad1e19
barteklecki <leckibartek@gmail.com>
2020-07-10 09:14:36 +0200

    refactor(reducer): chang variables names to more clair

--- src/features/display/displaySlice.js
+++ src/features/display/displaySlice.js
@@ -6,33 +6,33 @@ export const displaySlice = createSlice({
     initialState: {
         nextOperationFlag: false,
         mathOperations: [],
-        result: 0,
+        supportVar: 0,
         displayNumber: '0',
     },
     reducers: {
         add: state => {
             state.mathOperations.push(state.displayNumber);
-            state.result = resultCalc(state.mathOperations);
-            state.displayNumber = state.result + '';
+            state.supportVar = resultCalc(state.mathOperations);
+            state.displayNumber = state.supportVar + '';
             state.mathOperations.push('+');
             state.nextOperationFlag = true;
         },
         subtract: state => {
             state.mathOperations.push(state.displayNumber);
-            state.result = resultCalc(state.mathOperations);
-            state.displayNumber = state.result + '';
+            state.supportVar = resultCalc(state.mathOperations);
+            state.displayNumber = state.supportVar + '';
             state.mathOperations.push('-');
             state.nextOperationFlag = true;
         },
         multiply: state => {
             state.mathOperations.push(state.displayNumber);
-            state.result = resultCalc(state.mathOperations);
+            state.supportVar = resultCalc(state.mathOperations);
             state.mathOperations.push('*');
             state.nextOperationFlag = true;
         },
         divide: state => {
             state.mathOperations.push(state.displayNumber);
-            state.result = resultCalc(state.mathOperations);
+            state.supportVar = resultCalc(state.mathOperations);
             state.mathOperations.push('/');
             state.nextOperationFlag = true;
         },
@@ -53,20 +53,20 @@ export const displaySlice = createSlice({
         },
         equals: state => {
             state.mathOperations.push(state.displayNumber);
-            state.result = resultCalc(state.mathOperations);
-            state.displayNumber = state.result+'';
+            state.supportVar = resultCalc(state.mathOperations);
+            state.displayNumber = state.supportVar+'';
             state.mathOperations = [];
             state.nextOperationFlag = false;
         },
         addComma: state => {
             if (state.displayNumber.indexOf('.') === -1) {
-                let perc = '';
+                let trailingChar = '';
                 if( state.displayNumber.slice(-1) === '%' ) {
-                    perc = '%';
+                    trailingChar = '%';
                     state.displayNumber = state.displayNumber.slice(0, -1);
                 }
                 state.displayNumber += '.';
-                state.displayNumber += perc;
+                state.displayNumber += trailingChar;
             }
         },
         addDigit: (state, action) => {
@@ -74,18 +74,18 @@ export const displaySlice = createSlice({
                 state.displayNumber = '';
                 state.nextOperationFlag = false;
             }
-            let perc = '';
+            let trailingChar = '';
             if( state.displayNumber.slice(-1) === '%' ) {
-                perc = '%';
+                trailingChar = '%';
                 state.displayNumber = state.displayNumber.slice(0, -1);
             }
-            state.displayNumber = +(state.displayNumber + action.payload) + perc;
+            state.displayNumber = +(state.displayNumber + action.payload) + trailingChar;
             return
         },
         reset: state => {
             state.nextOperationFlag = false;
             state.mathOperations = [];
-            state.result = 0;
+            state.supportVar = 0;
             state.displayNumber = '0';
         },
 
@@ -107,5 +107,5 @@ export const {
 export default displaySlice.reducer;
 
 export const selectOperations = state => state.display.mathOperations;
-export const selectResult = state => state.display.result;
+export const selectSupportVar = state => state.display.supportVar;
 export const selectDisplayNumber = state => state.display.displayNumber;
